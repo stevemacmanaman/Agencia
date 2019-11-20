cb647c030e1e54725fee7b25f433a3cc2b6ac810
unknown <first_macmanaman@hotmail.com>
2019-11-20 09:49:04 -0600

    solucion al puerto

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ app.listen(3000);*/
 /*puerto y host para la app.*/
 
 const host = process.env.HOST || '0.0.0.0';
-const port = process.env.POST ||3000;
+const port = process.env.PORT || 3000;
 app.listen(port, host, ()=>{
     console.log('el servidor esta funcionando');
 });
